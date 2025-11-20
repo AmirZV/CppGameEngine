c7bc9ec455e2e58cd63a00bfe6aaaf1933f58734
Amir Zand <AmirZV@outlook.com>
2025-11-20 16:14:38 +0100

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,49 @@
 #include <imgui.h>
 #include <imgui-SFML.h>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML 3 + ImGui");
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "CG Engine");
     window.setFramerateLimit(60);
 
-    if (!ImGui::SFML::Init(window)) {
+    if (!ImGui::SFML::Init(window))
+    {
         return -1;
     }
 
     sf::Clock clock;
 
-    while (window.isOpen()) {
-        // SFML 3 event loop
-        while (auto optEvent = window.pollEvent()) {
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional optEvent = window.pollEvent())
+        {
             const sf::Event& event = *optEvent;
 
             ImGui::SFML::ProcessEvent(window, event);
 
-            // ---- NEW SFML 3 way to detect Close event ----
+            // "close requested" event: we close the window
             if (event.is<sf::Event::Closed>())
                 window.close();
         }
 
         ImGui::SFML::Update(window, clock.restart());
 
-        ImGui::Begin("Hello");
-        ImGui::Text("This is ImGui + SFML 3!");
+        ImGui::Begin("Menu");
+        ImGui::Text("Play with the Engine!");
         ImGui::End();
 
-        window.clear(sf::Color{20, 20, 20});
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        //window.draw(...);
         ImGui::SFML::Render(window);
+
+        // end the current frame
         window.display();
     }
 
